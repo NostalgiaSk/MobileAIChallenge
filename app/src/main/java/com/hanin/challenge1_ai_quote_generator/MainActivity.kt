cf47008e90aac3c228366b616add684843f05478
Nostalgia <userthree@xtendplex.com>
2025-10-22 00:06:10 +0200

    Challenge 1 : first commit

---
+++ app/src/main/java/com/hanin/challenge1_ai_quote_generator/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.hanin.challenge1_ai_quote_generator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hanin.challenge1_ai_quote_generator.ui.theme.Challenge1aiquotegeneratorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Challenge1aiquotegeneratorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Challenge1aiquotegeneratorTheme {
+        Greeting("Android")
+    }
+}
